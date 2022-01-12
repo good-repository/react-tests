a7d3b24dc70322b272a5fd54e20bf2f7bdfb043b
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-01-12 12:40:55 -0300

    hide toppings header on summary if no toppings are selected

--- sundaes-on-demand/src/pages/summary/OrderSummary.jsx
+++ sundaes-on-demand/src/pages/summary/OrderSummary.jsx
@@ -20,8 +20,12 @@ export default function OrderSummary({ setOrderPhase }) {
       <h1>Order Summary</h1>
       <h2>Scoops: {orderDetails.totals.scoops}</h2>
       <ul>{scoopList}</ul>
-      <h2>Toppings: {orderDetails.totals.toppings}</h2>
-      <ul>{toppingList}</ul>
+      {toppingsArray.length > 0 && (
+        <>
+          <h2>Toppings: {orderDetails.totals.toppings}</h2>
+          <ul>{toppingList}</ul>
+        </>
+      )}
       <SummaryForm setOrderPhase={setOrderPhase} />
     </div>
   );
--- sundaes-on-demand/src/tests/orderPhase.test.jsx
+++ sundaes-on-demand/src/tests/orderPhase.test.jsx
@@ -94,3 +94,35 @@ test("Order phases for happy path", async () => {
   await screen.findByRole("spinbutton", { name: "Vanilla" });
   await screen.findByRole("checkbox", { name: "Cherries" });
 });
+
+test("Toppings header is not on summary page if no toppings ordered", async () => {
+  render(<App />);
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+
+  const chocolateInput = screen.getByRole("spinbutton", { name: "Chocolate" });
+  userEvent.clear(chocolateInput);
+  userEvent.type(chocolateInput, "2");
+
+  const orderSummaryButton = screen.getByRole("button", {
+    name: /order sundae/i,
+  });
+  userEvent.click(orderSummaryButton);
+
+  const summaryHeading = screen.getByRole("heading", { name: "Order Summary" });
+  expect(summaryHeading).toBeInTheDocument();
+
+  const scoopsHeading = screen.getByRole("heading", { name: "Scoops: $6.00" });
+  expect(scoopsHeading).toBeInTheDocument();
+
+  const toppingsHeading = screen.queryByRole("heading", { name: /toppings/i });
+  expect(toppingsHeading).not.toBeInTheDocument();
+
+  // check summary option items
+  expect(screen.getByText("1 Vanilla")).toBeInTheDocument();
+  expect(screen.getByText("2 Chocolate")).toBeInTheDocument();
+});
