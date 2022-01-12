d37ffb1d70849bf7b15384d01c7f23edfbd28744
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-01-12 17:13:41 -0300

    Handle error on order confirmation page

--- sundaes-on-demand/src/pages/confirmation/OrderConfirmation.jsx
+++ sundaes-on-demand/src/pages/confirmation/OrderConfirmation.jsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "react-bootstrap/Button";
 import { useOrderDetails } from "../../contexts/OrderDetails";
+import AlertBanner from "../common/AlertBanner";
 
 export default function OrderConfirmation({ setOrderPhase }) {
   const [, , resetOrder] = useOrderDetails();
   const [orderNumber, setOrderNumber] = useState(null);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
@@ -15,9 +17,7 @@ export default function OrderConfirmation({ setOrderPhase }) {
       .then((response) => {
         setOrderNumber(response.data.orderNumber);
       })
-      .catch((error) => {
-        // TODO: handle error here
-      });
+      .catch((error) => setError(true));
   }, []);
 
   function handleClick() {
@@ -26,6 +26,10 @@ export default function OrderConfirmation({ setOrderPhase }) {
 
     // send back to order page
     setOrderPhase("inProgress");
+  }
+
+  if (error) {
+    return <AlertBanner />;
   }
 
   if (orderNumber) {
