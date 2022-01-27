a04e5e841248cc69fe6144bb55397f00b9a2eee2
Guilherme Fünkler Borelli <borelli.guilherme@gmail.com>
2022-01-27 00:16:21 -0300

    remove useless imports and test hold cancellations

--- advanced-tests/base-app/client/src/features/tickets/redux/ticketSaga.test.ts
+++ advanced-tests/base-app/client/src/features/tickets/redux/ticketSaga.test.ts
@@ -1,28 +1,14 @@
-import { PayloadAction } from "@reduxjs/toolkit";
-import axios, { CancelTokenSource } from "axios";
-import { SagaIterator } from "redux-saga";
-import {
-  call,
-  cancel,
-  cancelled,
-  put,
-  race,
-  select,
-  take,
-  takeEvery,
-} from "redux-saga/effects";
+import axios from "axios";
 import { expectSaga } from "redux-saga-test-plan";
 import * as matchers from "redux-saga-test-plan/matchers";
 import { StaticProvider, throwError } from "redux-saga-test-plan/providers";
 
-import { HoldReservation } from "../../../../../shared/types";
 import {
   holdReservation,
   purchasePayload,
   purchaseReservation,
 } from "../../../test-utils/fake-data";
 import { showToast } from "../../toast/redux/toastSlice";
-import { ToastOptions } from "../../toast/types";
 import {
   cancelPurchaseServerCall,
   releaseServerCall,
@@ -37,9 +23,6 @@ import {
 } from "./ticketSaga";
 import {
   endTransaction,
-  holdTickets,
-  PurchasePayload,
-  ReleasePayload,
   resetTransaction,
   selectors,
   startTicketAbort,
@@ -152,12 +135,36 @@ describe("purchase flow", () => {
     return expectSaga(purchaseTickets, purchasePayload, cancelSource)
       .provide(networkProviders)
       .call(reserveTicketServerCall, purchaseReservation, cancelSource.token)
-      .call(releaseServerCall, holdReservation)
       .put(showToast({ title: "tickets purchased", status: "success" }))
+      .call(releaseServerCall, holdReservation)
       .put(endTransaction())
       .not.call.fn(cancelSource.cancel)
       .not.call.fn(cancelPurchaseServerCall)
       .not.put(showToast({ title: "purchase canceled", status: "warning" }))
       .run();
   });
+});
+
+describe("hold cancellations", () => {
+  // will interpolate when https://github.com/facebook/jest/pull/11388 is realeased
+  // test.each([
+  //   { name: "cancel", actionCreator: startTicketRelease },
+  //   { name: "abort", actionCreator: startTicketAbort },
+  // ])(
+  //   "cancel hold and reset ticket transaction on $name",
+  //   ({ actionCreator }) => {
+  test.each([[startTicketRelease], [startTicketAbort]])(
+    `cancel hold and reset ticket transaction on %s`,
+    (actionCreator) => {
+      return expectSaga(ticketFlow, holdAction)
+        .provide(networkProviders)
+        .call.fn(reserveTicketServerCall)
+        .dispatch(
+          actionCreator({ reservation: holdReservation, reason: "test" })
+        )
+        .put(showToast({ title: "test", status: "warning" }))
+        .call(cancelTransaction, holdReservation)
+        .run();
+    }
+  );
 });
